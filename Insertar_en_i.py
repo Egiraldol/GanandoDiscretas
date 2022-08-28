35025d6ca9a17304f65b5426fff1285cce47f590
gutim1011 <104235522+gutim1011@users.noreply.github.com>
2022-08-27 20:07:45 -0500

    Update Insertar_en_i.py

--- Insertar_en_i.py
+++ Insertar_en_i.py
@@ -14,3 +14,28 @@ def insertarAux(head: Node, valor: int) -> Node:
     NodoCabeza = Node(valor)
     NodoCabeza.next = head
     return NodoCabeza
+'''
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def insertar(head: Node, valor, posI):
+    return posicionAux(head,valor,posI,1)
+def posicionAux(head:Node,valor:int,i:int,j:int,aux=0)->Node:
+    if head==None:
+        newHead = Node(valor)
+        newHead.next= None
+        return newHead
+    else:
+        if j == i - 1:
+            aux = head.next
+            head.next = valor
+        if j == i:
+            head.val = valor
+            head.next = aux
+        else:
+            return posicionAux(head.next,valor,i,j+1)
+        return head
+
+'''
