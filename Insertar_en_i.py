204975d6779510aec7a69376478814ad61e43931
Egiraldol <104527878+Egiraldol@users.noreply.github.com>
2022-08-27 20:03:05 -0500

    Update Insertar_en_i.py

--- Insertar_en_i.py
+++ Insertar_en_i.py
@@ -1,1 +1,16 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
 
+def insertar(head: Node, valor, posI):
+    if posI == 1:
+        cambio = insertarAux(head, valor)
+        return cambio
+    else:
+        return insertar(head.next, valor, posI-1)
+
+def insertarAux(head: Node, valor: int) -> Node:
+    NodoCabeza = Node(valor)
+    NodoCabeza.next = head
+    return NodoCabeza
